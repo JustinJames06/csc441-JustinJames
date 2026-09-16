3bf7c6648c2cbcb04b3d21e8ea526dbde062be9c
Justin James <justinjames162@gmail.com>
2026-09-16 18:00:46 -0400

    Adding classwork for week4wed

---
+++ csc441-kotlin/src/week4wed/ClassWork.kt
@@ -0,0 +1,72 @@
+package week4wed
+
+fun main() {
+    println("---Step 1: Function returning nothing---")
+    todaysExcercise()
+
+    println("---Step 2: Function returning a value---")
+    println(todaysExercise1())
+
+    println("---Step 3: Function returning a parameter---")
+    println(todaysExercise2(3))
+    println(todaysExercise2(9))
+
+    println("---Step 4: the short form---")
+    println(todaysExercise3(3))
+
+    println("---Step 5: a default value---")
+    println(todaysExercise4())
+    println(todaysExercise4(2))
+
+    println("---Step 6: two parameters, and naming them---")
+    println(todaysExercise5(2, "Justin"))
+    println(todaysExercise5(name = "justin"))
+}
+
+fun todaysExcercise() {
+    println("Push-ups")
+}
+fun todaysExercise1(): String {
+    return "Push-ups"
+}
+
+fun todaysExercise2(dayNumber: Int): String {
+    return when (dayNumber) {
+        1 -> "Push-ups"
+        2 -> "Running"
+        3 -> "Swimming"
+        4 -> "Cycling"
+        5 -> "Gym"
+        else -> "Rest day"
+    }
+}
+
+fun todaysExercise3(dayNumber: Int): String = when (dayNumber) {
+    1 -> "Push-ups"
+    2 -> "Running"
+    3 -> "Swimming"
+    4 -> "Cycling"
+    5 -> "Gym"
+    else -> "Rest day"
+}
+
+fun todaysExercise4(dayNumber: Int = 1): String = when (dayNumber) {
+    1 -> "Push-ups"
+    2 -> "Running"
+    3 -> "Swimming"
+    4 -> "Cycling"
+    5 -> "Gym"
+    else -> "Rest day"
+}
+
+fun todaysExercise5(dayNumber: Int = 1, name: String = "you"): String {
+    val excercise = when (dayNumber) {
+        1 -> "Push-ups"
+        2 -> "Running"
+        3 -> "Swimming"
+        4 -> "Cycling"
+        5 -> "Gym"
+        else -> "Rest day"
+    }
+    return "$name is $excercise"
+}
